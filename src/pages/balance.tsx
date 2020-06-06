6cbcf4794f7d33eeff7f719a89379d7bd02d63de
Rath-san <rim.kozlowski@gmail.com>
2020-06-06 16:37:34 +0200

    Adding animations for yoga

--- src/pages/balance.tsx
+++ src/pages/balance.tsx
@@ -4,6 +4,7 @@ import { Hero } from '../components/Hero';
 import { BalanceImage } from '../components/images/BalanceImage';
 import { Layout } from '../components/Layout';
 import '../styles/styles.scss';
+import BALANCE_PAGE from '../data/balance.json';
 
 interface ScoutsPageProps {
     data: {
@@ -20,7 +21,7 @@ export default (props: ScoutsPageProps) => {
 
     return (
         <Layout>
-            <Hero name="Balance" tagline={'test'} style={'is-seconadry'} image={<BalanceImage />}/>
+            <Hero name="Balance" tagline={BALANCE_PAGE.header.text} style={'is-seconadry'} image={<BalanceImage />}/>
         </Layout>
     );
 };
